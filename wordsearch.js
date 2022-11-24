3cf726fc30c9df66d61667150a9b81bd3e37dffd
Lyon <lyon.b.lee@gmail.com>
2022-11-23 19:08:56 -0500

    wordsearch.js added, only vertical/horizontal search available

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
+const transpose = require("../../async/matrix_transposition");
+const eqArrays = require("../../lotide/eqArrays")
+
 const wordSearch = (letters, word) => { 
+
+    if (eqArrays(letters, [])) {
+
+        return false;
+    
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
+       
         if (l.includes(word)) return true
     }
+
+    let newLetters = transpose(letters);
+
+    const verticalJoin = newLetters.map(rs => rs.join(''))
+
+    for (r of verticalJoin) {
+       
+        if (r.includes(word)) return true
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
